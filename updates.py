b382052eb11262630c0028cbf17b5ffb7ad5d150
clux <analsandblaster@gmail.com>
2017-04-16 20:27:09 +0100

    function to check for blackbox updates

--- updates.py
+++ updates.py
@@ -15,26 +15,25 @@ def get_node_version():
 
     return last_lts['version']
 
-def get_sublime_build():
-    '''Hacky parsing of sublime/3 page to get latest build'''
-    resp = requests.get("http://www.sublimetext.com/3")
-    match = re.search(r'Build (\d{4,5})', resp.text)
-    return int(match.group(1))
-
-
 def get_stable_rust_version():
     '''Hacky parsing of rust-lang download page to get latest stable version'''
     resp = requests.get("https://www.rust-lang.org/en-US/install.html")
     match = re.search(r'(\d+\.\d+\.\d+)', resp.text)
     return match.group(1)
 
+def get_blackbox_release():
+    '''Fetch latest github release of StackExchange/blackbox'''
+    resp = requests.get("https://api.github.com/repos/StackExchange/blackbox/tags")
+    latest = resp.json()[0]
+    # NB: could use git, instead maybe, that way we have a sha (it's in the resp)
+    return latest['name'][1:] # slice away leading v
+
 if __name__ == '__main__':
     VERSIONS = None
     with open(VERSIONS_FILE, 'r') as file:
         VERSIONS = yaml.safe_load(file)
     print(VERSIONS)
-
-    VERSIONS['subl_build'] = get_sublime_build()
+    VERSIONS['blackbox_version'] = get_blackbox_release()
     VERSIONS['rust_ver'] = get_stable_rust_version()
     VERSIONS['node'] = get_node_version()
 
