759dbafe12f4db60648713bcdcd70ac490aee091
clux <analsandblaster@gmail.com>
2017-04-19 23:19:28 +0100

    generalize publishing script for now

---
+++ publish.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+if [ ! -d "$1" ]; then
+  echo "No such package $1"
+  exit 1
+fi
+
+cd "$1"
+
+git clone "ssh://aur@aur.archlinux.org/$1.git"
+cp PKGBUILD "$1/"
+makepkg --printsrcinfo > "$1/.SRCINFO"
+cd "$1"
+git add PKGBUILD .SRCINFO
+git clux
+# review and push
+
+# look over https://wiki.archlinux.org/index.php/Arch_User_Repository#Submitting_packages again
