bdf2d662db1cc4960df4e8ba844dd70788a64615
clux <analsandblaster@gmail.com>
2017-05-27 00:24:07 +0100

    update pins + script updates of personals

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # pkgbuilds
 Personal experiments, AUR pins, and the occasional original AUR package.
 
+## Updating
+Update pins and verify sanity of updated `PKGBUILD` files.
+
+```sh
+git submodule update --recursive --remote
+./updates.py
+```
+
 ## Submitted to AUR
 - [blackbox-vcs](https://aur.archlinux.org/packages/blackbox-vcs/)
 
--- updates.py
+++ updates.py
@@ -13,7 +13,7 @@ def get_node_version():
     resp = requests.get("https://nodejs.org/download/release/index.json")
     last_lts = [v for v in resp.json() if v['lts']][0]
 
-    return last_lts['version']
+    return last_lts['version'][1:] # remove leading 'v'
 
 def get_stable_rust_version():
     '''Hacky parsing of rust-lang download page to get latest stable version'''
@@ -28,15 +28,25 @@ def get_blackbox_release():
     # NB: could use git, instead maybe, that way we have a sha (it's in the resp)
     return latest['name'][1:] # slice away leading v
 
+def bump_pkgver(pkg, ver):
+    '''Update pkgver line in PKGBUILD for a subdirectory'''
+    cmd = "sed -i s/pkgver=\.*/pkgver={}/ ./{}/PKGBUILD".format(ver, pkg)
+    subprocess.Popen(cmd, shell=True)
+
 if __name__ == '__main__':
     VERSIONS = None
     with open(VERSIONS_FILE, 'r') as file:
         VERSIONS = yaml.safe_load(file)
     print(VERSIONS)
-    VERSIONS['blackbox_version'] = get_blackbox_release()
-    VERSIONS['rust_ver'] = get_stable_rust_version()
+    VERSIONS['blackbox'] = get_blackbox_release()
+    VERSIONS['rust'] = get_stable_rust_version()
     VERSIONS['node'] = get_node_version()
 
+    # Write it down in a file for personal reference
     with open(VERSIONS_FILE, 'w') as file:
         pyaml.dump(VERSIONS, file, explicit_start=True)
         print("Updated {}".format(VERSIONS_FILE))
+
+    # More importantly propagate versions to PKGBUILD files
+    bump_pkgver('nodejs-lts', VERSIONS['node'])
+    bump_pkgver('blackbox-vcs', VERSIONS['blackbox_version'])
