ba33575f1645e4ddfb67563c48bef9b2c70caaf5
clux <analsandblaster@gmail.com>
2017-08-05 23:53:23 +0100

    note to self v2

--- publish.sh
+++ publish.sh
@@ -14,7 +14,7 @@ makepkg --printsrcinfo > "$1/.SRCINFO"
 cd "$1"
 git add PKGBUILD .SRCINFO
 git clux
-# review, try to build it, update sha256sum of new sources, and push
+# review, verify sha256sum got updated, try to build it, then push
 
 
 # look over https://wiki.archlinux.org/index.php/Arch_User_Repository#Submitting_packages again
