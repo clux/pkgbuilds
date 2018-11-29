886caa138210b23e2203e0db5470b1a1f9ee2032
clux <sszynrae@gmail.com>
2018-11-29 13:33:01 +0000

    update publish script

--- publish.sh
+++ publish.sh
@@ -8,13 +8,11 @@ fi
 
 cd "$1"
 
-git clone "ssh://aur@aur.archlinux.org/$1.git"
-cp PKGBUILD "$1/"
-makepkg --printsrcinfo > "$1/.SRCINFO"
-cd "$1"
+makepkg --printsrcinfo > .SRCINFO
 git add PKGBUILD .SRCINFO
 git clux
-# review, verify sha256sum got updated, try to build it, then push
+git commit -m "bump blackbox pin"
+# review, verify sha256sum got updated, try to build it, then push manually
 
 
 # look over https://wiki.archlinux.org/index.php/Arch_User_Repository#Submitting_packages again
